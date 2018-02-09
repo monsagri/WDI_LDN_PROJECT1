1fd69770e58cb3cc33cf57fcb37d3efb83672437
Fabian Feldberg <feldbergf@gmail.com>
2018-02-09 15:01:10 +0000

    added movement

--- src/js/app.js
+++ src/js/app.js
@@ -8,6 +8,7 @@ let $newGame = null;
 let $playerDiv = null;
 let playerLocation = null;
 let boardSize = 0;
+let boardLength = 0;
 
 //Check for DOM loaded
 $(function() {
@@ -34,28 +35,60 @@ function init(){
 
 function createMap(size) {
   boardSize = size * size;
+  boardLength = size;
   for (let i = 0; i < size*size; i++){
     $board.append($(`<div style="width: ${(100 / size )}%; height: ${(100 / size )}%; border: 1px solid black;" data-location="${i}";></div>`));
   }
 }
 function newGame() {
   const startingLocation = Math.floor((Math.random() * boardSize));
+  // Clear board of old player sprite
   $('div').removeClass('player');
+  // Set New Player location Div
   $(`[data-location="${startingLocation}"]`).toggleClass('player');
   $playerDiv = $('.player');
+  // Save Player Location to avoid DOM reference
   playerLocation = $playerDiv.data('location');
   console.log(playerLocation);
+
+  // Toggle Event Listeners for movement
+  $('body').keydown(move);
 }
 
-function moveUp() {
+function move(e) {
+  console.log(e.keyCode);
+  const key = e.keyCode;
+  if (key === 87) moveUp();
+  if (key === 83) moveDown();
+  if (key === 65) moveLeft();
+  if (key === 68) moveRight();
 
+}
+function moveUp() {
+  console.log('moving up');
+  $playerDiv.removeClass('player');
+  $(`[data-location="${playerLocation - boardLength}"]`).toggleClass('player');
+  playerLocation -= boardLength;
+  $playerDiv = $('.player');
 }
 function moveDown() {
-
+  console.log('moving down');
+  $playerDiv.removeClass('player');
+  $(`[data-location="${playerLocation + boardLength}"]`).toggleClass('player');
+  playerLocation += boardLength;
+  $playerDiv = $('.player');
 }
 function moveLeft() {
-
+  console.log('moving left');
+  $playerDiv.removeClass('player');
+  $(`[data-location="${playerLocation - 1}"]`).toggleClass('player');
+  playerLocation -= 1;
+  $playerDiv = $('.player');
 }
 function moveRight() {
-
+  console.log('moving right');
+  $playerDiv.removeClass('player');
+  $(`[data-location="${playerLocation + 1}"]`).toggleClass('player');
+  playerLocation += 1;
+  $playerDiv = $('.player');
 }
