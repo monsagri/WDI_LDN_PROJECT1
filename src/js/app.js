db2af115d7a8994c0dd961fe0cbb8210c745cdde
Fabian Feldberg <feldbergf@gmail.com>
2018-02-09 14:07:08 +0000

    Added newgame function, randomly places player on board

--- src/js/app.js
+++ src/js/app.js
@@ -4,7 +4,9 @@ console.log('JS locked and loaded');
 // Declare global functions
 
 let $board = null;
-
+let $newgame = null;
+let $playerdiv = null;
+let boardSize = 0;
 
 //Check for DOM loaded
 $(function() {
@@ -19,16 +21,24 @@ function init(){
   console.log('DOM loaded');
   // grab DOM-related variables
   $board = $('.gameboard');
+  $playerdiv = $('.player');
+  $newgame = $('#newgame');
   // run Functions
   createMap(5);
-
   // add Event listeners
+  $newgame.on('click', newGame);
 }
 
 // Declare DOM-related Functions
 
 function createMap(size) {
+  boardSize = size * size;
   for (let i = 0; i < size*size; i++){
-    $board.append($(`<div style="width: ${(100 / size )}%; height: ${(100 / size )}%; background-color: blue; border: 1px solid black;"></div>`));
+    $board.append($(`<div style="width: ${(100 / size )}%; height: ${(100 / size )}%; border: 1px solid black;" data-location="${i}";></div>`));
   }
+}
+function newGame() {
+  const startingLocation = Math.floor((Math.random() * boardSize));
+  $('div').removeClass('player');
+  $(`[data-location="${startingLocation}"]`).toggleClass('player');
 }
