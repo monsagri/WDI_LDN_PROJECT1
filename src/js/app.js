3060b64dca84aa947c944464812151f68307f01e
Fabian Feldberg <feldbergf@gmail.com>
2018-02-09 13:18:56 +0000

    Added Map Creation Function

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,34 @@
-[10,20,30].forEach(num => console.log(num));
+console.log('JS locked and loaded');
+// Declare global variables
+
+// Declare global functions
+
+let $board = null;
+
+
+//Check for DOM loaded
+$(function() {
+
+
+  // run DOM related functions
+  init();
+});
+
+
+function init(){
+  console.log('DOM loaded');
+  // grab DOM-related variables
+  $board = $('.gameboard');
+  // run Functions
+  createMap(5);
+
+  // add Event listeners
+}
+
+// Declare DOM-related Functions
+
+function createMap(size) {
+  for (let i = 0; i < size*size; i++){
+    $board.append($(`<div style="width: ${(100 / size )}%; height: ${(100 / size )}%; background-color: blue; border: 1px solid black;"></div>`));
+  }
+}
