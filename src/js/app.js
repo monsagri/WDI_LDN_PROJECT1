299bdb32dae9325283ce9317958f896dace9ccb9
Fabian Feldberg <feldbergf@gmail.com>
2018-02-12 15:32:00 +0000

    added logic for combat with enemies -missing elements for damage/health

--- src/js/app.js
+++ src/js/app.js
@@ -7,9 +7,10 @@ let $board = null;
 let $newGame = null;
 let $playerDiv = null;
 let $soundButton = null;
+let $healthBar = null;
 let audio = null;
 
-let level = 0;
+let level = 1;
 let startingLocation = 0;
 let treasureLocation = 0;
 let enemyLocations = [];
@@ -17,6 +18,8 @@ let playerLocation = null;
 let boardSize = 0;
 let boardHeight = 0;
 let boardWidth = 0;
+
+let health = 3;
 const walls = [[2,4,9,11,12,14,18,24,27,32,33,37,38,46,47,52,59,60,62,63,64,66,72,76,79,84,86,91,92,94],[3,6,9,15,23,28,33,34,35,36,37,41,44,47,57,59,63,64,66,67,72,77,82,91,93,99,105,109,110,111,114,120,122,123,126,131,135,138,143,158,160,161,163,164,165,166,168,171,178,188,192,197],[3, 33, 63, 93, 123, 122, 120, 181, 211, 212, 213, 214, 243, 273, 303, 333, 363, 393, 423, 453, 483, 573, 271, 331, 391, 451, 541, 542, 543, 336, 366, 396, 426, 456, 486, 516, 337, 367, 397, 427, 457, 487, 517, 215, 217, 218, 219, 220, 250, 280, 340, 370, 400, 460, 520, 550, 580, 461, 462, 463, 492, 522, 584, 554, 464, 466, 465, 556, 558, 588, 561, 591, 559, 467, 469, 470, 471, 472, 502, 532, 562, 592, 185, 125, 126, 127, 97, 67, 37, 68, 69, 70, 40, 10, 71, 72, 102, 132, 221, 133,35, 105, 136, 137, 138, 139, 140, 141, 111, 81, 51, 21, 223, 224, 225, 196, 168, 200, 226, 227, 229, 228, 230, 202, 172, 261, 260, 262, 142, 282, 312, 342, 372, 402, 403, 404, 405, 406, 376, 346, 316, 286, 285, 284, 314, 344, 408, 378, 348, 318, 291, 321, 441, 380, 379, 382, 383, 384, 386, 387, 388, 417, 447, 477, 507, 537, 567, 594, 564, 565, 535, 505, 474, 475, 508, 509, 354, 324, 294, 264, 234, 204, 174, 144, 114, 84, 52, 22, 23, 24, 25, 26, 56, 86, 146, 147, 148, 118, 88,119, 149, 205, 206, 207, 208, 299, 298, 297, 296, 327, 328, 235, 267]];
 let visibleSquares = [];
 let stepsTaken = 0;
@@ -43,6 +46,7 @@ function init(){
   $newGame = $('#newgame');
   $soundButton = $('#sound');
   audio = document.querySelector('audio');
+  $healthBar = $('#healthbar');
   // run Functions
 
   // add Event listeners
@@ -66,15 +70,15 @@ function createMap(height,width) {
   }
   addWalls();
   console.log($('.area'));
-  $('.area').on('click', makeWall);
+  // $('.area').on('click', makeWall);
 }
-function makeWall() {
-  console.log('I\'m a wall now.');
-  $(this).removeClass('floor');
-  $(this).addClass('wall');
-  console.log($(this).data('location'));
-  walls[level].push($(this).data('location'));
-}
+// function makeWall() {
+//   console.log('I\'m a wall now.');
+//   $(this).removeClass('floor');
+//   $(this).addClass('wall');
+//   console.log($(this).data('location'));
+//   walls[level].push($(this).data('location'));
+// }
 function addWalls() {
   walls[level].forEach((location) => {
     $(`[data-location="${location}"]`).removeClass('floor');
@@ -140,6 +144,9 @@ function move(e) {
   checkForWin();
 }
 function moveUp() {
+  if (enemyLocations.includes((playerLocation - boardHeight))) {
+    if (combat() !== 'win') return;
+  }
   if ((playerLocation - boardHeight) < 0 || walls[level].includes((playerLocation - boardHeight)) ) {
     console.log('That\'s a wall.');
   } else {
@@ -153,6 +160,9 @@ function moveUp() {
   }
 }
 function moveDown() {
+  if (enemyLocations.includes((playerLocation + boardHeight))) {
+    if (combat() !== 'win') return;
+  }
   if (playerLocation === (boardSize - boardHeight)||(playerLocation + boardHeight) > boardSize || walls[level].includes((playerLocation + boardHeight))) {
     console.log('That\'s a wall.');
   } else {
@@ -166,6 +176,9 @@ function moveDown() {
   }
 }
 function moveLeft() {
+  if (enemyLocations.includes((playerLocation - 1))) {
+    if (combat() !== 'win') return;
+  }
   if ((playerLocation === 0 || playerLocation % boardHeight === 0 || walls[level].includes((playerLocation - 1)))) {
     console.log('That\'s a wall.');
   } else {
@@ -180,8 +193,9 @@ function moveLeft() {
   }
 }
 function moveRight() {
-  // This stops movement through top right corner, but also through center of map
-  // (playerLocation % boardWidth === boardWidth - 1) ||
+  if (enemyLocations.includes((playerLocation + 1))) {
+    if (combat() !== 'win') return;
+  }
   if ((playerLocation % boardHeight === boardHeight - 1) || walls[level].includes((playerLocation + 1))) {
     console.log('That\'s a wall.');
   } else {
@@ -215,6 +229,16 @@ function visionLeft() {
 function visionRight() {
   if (playerLocation % boardHeight === boardHeight - 1) return;
   else return playerLocation + 1;
+}
+function combat() {
+  console.log('fight the beastie');
+  health -= 1;
+  console.log($('#healthbar img:last-child'));
+  $('#healthbar img:last-child').remove();
+  if (health > 0) return 'win';
+  window.alert('You died!');
+  reset();
+  newGame();
 }
 function checkForWin() {
   if (playerLocation === treasureLocation) {
