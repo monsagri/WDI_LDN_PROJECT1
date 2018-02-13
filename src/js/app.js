9fa2ef2f2fa68b1b67e9d219fbfa263d8263070a
Fabian Feldberg <feldbergf@gmail.com>
2018-02-13 12:23:50 +0000

    added constructor functions for enemies, need to add movement -refactoring from here

--- src/js/app.js
+++ src/js/app.js
@@ -15,6 +15,7 @@ let level = 0;
 let startingLocation = 0;
 let doorLocation = 0;
 let enemyLocations = [];
+let enemies = [];
 let playerLocation = null;
 let boardSize = 0;
 let boardHeight = 0;
@@ -24,10 +25,10 @@ let health = 3;
 
 
 // Working on adding objects to refactor
-const player = {
+let player = {
   health: 3,
-  weapon: 'none',
-  armor: 'none',
+  weapon: 1,
+  armor: 0,
   items: []
 };
 
@@ -38,13 +39,71 @@ const moveKey = {
   68: moveRight
 };
 
+const mapDimensions = [[10,10],[20,10],[30,20]];
+const enemyDefinitions = [
+  {
+    type: 'rat',
+    health: 1,
+    attack: 1,
+    armor: 0,
+    speed: 0,
+    imageSrc: '/images/giant_rat.gif'
+  },
+  {
+    type: 'goblin',
+    health: 2,
+    attack: 1,
+    armor: 0,
+    speed: 1000,
+    imageSrc: '/images/goblin.png'
+  },
+  {
+    type: 'ogre',
+    health: 3,
+    attack: 2,
+    armor: 1,
+    speed: 2000,
+    imageSrc: '/images/giant-rat.gif'
+  }
+];
+class Enemy {
+  constructor(properties) {
+    Object.assign(this, properties);
+    this.location = getEnemyLocation();
+  }
+  attack() {
+    console.log(this.type + ' hits you for ' + (this.attack - player('armor') + ' damage.'));
+    player('health') - this.attack;
+    if (player('health') <= 0) {
+      console.log('You were killed by a ' + this.type);
+      return loseGame();
+    } else {
+      console.log(`You hit ${this.type} for ${player('attack')} damage!`);
+      this.health -= (player('attack') - this.armor);
+      if (this.health <= 0) {
+        //remove image from square
+        // move player to square
+        // console log kill message
+        //
+      } else {
+        // move player back to original square
+        // replace enemy image on its square
+      }
+    }
+  }
+  move() {
+    // copy movement commands from Player
+    //
+    // set Interval to trigger it regularly
+    //
+  }
+}
 
 
 const walls = [[2,4,9,11,12,14,18,24,27,32,33,37,38,46,47,52,59,60,62,63,64,66,72,76,79,84,86,91,92,94],[3,6,9,15,23,28,33,34,35,36,37,41,44,47,57,59,63,64,66,67,72,77,82,91,93,99,105,109,110,111,114,120,122,123,126,131,135,138,143,158,160,161,163,164,165,166,168,171,178,188,192,197],[3, 33, 63, 93, 123, 122, 120, 181, 211, 212, 213, 214, 243, 273, 303, 333, 363, 393, 423, 453, 483, 573, 271, 331, 391, 451, 541, 542, 543, 336, 366, 396, 426, 456, 486, 516, 337, 367, 397, 427, 457, 487, 517, 215, 217, 218, 219, 220, 250, 280, 340, 370, 400, 460, 520, 550, 580, 461, 462, 463, 492, 522, 584, 554, 464, 466, 465, 556, 558, 588, 561, 591, 559, 467, 469, 470, 471, 472, 502, 532, 562, 592, 185, 125, 126, 127, 97, 67, 37, 68, 69, 70, 40, 10, 71, 72, 102, 132, 221, 133,35, 105, 136, 137, 138, 139, 140, 141, 111, 81, 51, 21, 223, 224, 225, 196, 168, 200, 226, 227, 229, 228, 230, 202, 172, 261, 260, 262, 142, 282, 312, 342, 372, 402, 403, 404, 405, 406, 376, 346, 316, 286, 285, 284, 314, 344, 408, 378, 348, 318, 291, 321, 441, 380, 379, 382, 383, 384, 386, 387, 388, 417, 447, 477, 507, 537, 567, 594, 564, 565, 535, 505, 474, 475, 508, 509, 354, 324, 294, 264, 234, 204, 174, 144, 114, 84, 52, 22, 23, 24, 25, 26, 56, 86, 146, 147, 148, 118, 88,119, 149, 205, 206, 207, 208, 299, 298, 297, 296, 327, 328, 235, 267]];
 let visibleSquares = [];
 let stepsTaken = 0;
 
-const mapDimensions = [[10,10],[20,10],[30,20]];
 
 //Check for DOM loaded
 $(function() {
@@ -80,9 +139,7 @@ function init(){
 
 function createMap(height,width) {
   $board.html('');
-  if (level > 0) {
-    $board.css({ 'height': '80vh', 'width': '80vw' });
-  }
+  $board.css({ 'height': '80vh', 'width': '80vw'});
   boardSize = height * width;
   boardHeight = height;
   boardWidth = width;
@@ -123,19 +180,42 @@ function getDoorLocation() {
   }
   $(`[data-location="${doorLocation}"]`).html('<img src="/images/door1.png">');
 }
+// function spawnEnemies(amount) {
+//   enemyLocations = [];
+//   for (let i = 0; i < amount; i++){
+//     enemyLocations.push(Math.floor((Math.random() * boardSize)));
+//     while (walls[level].includes(enemyLocations[i]) || enemyLocations[i] === playerLocation) {
+//       console.log('preventing spawning in a wall');
+//       enemyLocations[i] = (Math.floor((Math.random() * boardSize)));
+//     }
+//   }
+//   for (let j = 0; j < enemyLocations.length; j++) {
+//     $(`[data-location="${enemyLocations[j]}"]`).html('<img src="/images/giant_rat.gif">');
+//   }
+// }
 function spawnEnemies(amount) {
-  enemyLocations = [];
+  // create enemies
   for (let i = 0; i < amount; i++){
-    enemyLocations.push(Math.floor((Math.random() * boardSize)));
-    while (walls[level].includes(enemyLocations[i]) || enemyLocations[i] === playerLocation) {
-      console.log('preventing spawning in a wall');
-      enemyLocations[i] = (Math.floor((Math.random() * boardSize)));
-    }
+    // get random Enemy source
+    const enemyType = enemyDefinitions[0];
+    const enemy = new Enemy(enemyType);
+    enemies.push(enemy);
   }
-  for (let j = 0; j < enemyLocations.length; j++) {
-    $(`[data-location="${enemyLocations[j]}"]`).html('<img src="/images/giant_rat.gif">');
+  // Get images from enemy objects and place on grid
+  for (let j = 0; j < enemies.length; j++) {
+    $(`[data-location="${enemies[j]['location']}"]`).html(`<img src=${enemies[j]['imageSrc']}>`);
   }
 }
+function getEnemyLocation() {
+  let enemyLocation = Math.floor((Math.random() * boardSize));
+  while (walls[level].includes(enemyLocation) || enemyLocation === playerLocation || doorLocation === enemyLocation) {
+    console.log('preventing spawning in a wall');
+    enemyLocation = (Math.floor((Math.random() * boardSize)));
+  }
+  enemyLocations.push[enemyLocation];
+  return enemyLocation;
+}
+
 function newGame() {
   deactivateMovement();
   createMap(mapDimensions[level][0],mapDimensions[level][1]);
@@ -231,6 +311,8 @@ function moveRight() {
     $playerDiv = $('.player');
   }
 }
+
+
 function changeVisibility() {
   visibleSquares = [];
   visibleSquares.push(playerLocation,visionTop(),visionBottom(),visionLeft(),visionRight());
