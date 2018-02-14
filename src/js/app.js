ec1f02708620884654626a149c4891756564bce3
Fabian Feldberg <feldbergf@gmail.com>
2018-02-14 10:14:26 +0000

    ensured that door can"t spawn too close to player and some cleanup

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 console.log('JS locked and loaded');
 // Declare global variables
 
-// Declare global functions
+// const libraries = require('./libraries');
 
 let $board = null;
 let $newGame = null;
@@ -17,11 +17,12 @@ let enemies = [];
 let enemyLocations = [];
 let items = [];
 let itemLocations = [];
-
 let boardSize = 0;
 let boardHeight = 0;
-const mapDimensions = [[10,10],[20,10],[30,20]];
+let visibleSquares = [];
+let stepsTaken = 0;
 
+const mapDimensions = [[10,10],[20,10],[30,20]];
 const characterDefinitions = [
   {
     type: 'player',
@@ -57,6 +58,84 @@ const characterDefinitions = [
     imageSrc: '/images/ogre1.png'
   }
 ];
+const itemDefinitions = [
+  {
+    name: 'door',
+    imageSrc: '/images/door1.png'
+  },
+  {
+    type: 'weapon',
+    name: 'dagger',
+    damage: 1,
+    imageSrc: '/images/dagger.png'
+  },
+  {
+    type: 'weapon',
+    name: 'sword',
+    damage: 2,
+    imageSrc: '/images/sword.png'
+  },
+  {
+    type: 'armor',
+    name: 'leather',
+    armor: 1,
+    imageSrc: '/images/leather_armor.png'
+  },
+  {
+    type: 'armor',
+    name: 'plate',
+    armor: 2,
+    imageSrc: '/images/plate_armor.png'
+  },
+  {
+    name: 'potion',
+    health: 1,
+    imageSrc: '/images/health1.png'
+  },
+  {
+    type: 'items',
+    name: 'coin',
+    wealth: 1,
+    imageSrc: 'images/coin1.png'
+  }
+
+];
+const walls = [[2,4,9,11,12,14,18,24,27,32,33,37,38,46,47,52,59,60,62,63,64,66,72,76,79,84,86,91,92,94],[3,6,9,15,23,28,33,34,35,36,37,41,44,47,57,59,63,64,66,67,72,77,82,91,93,99,105,109,110,111,114,120,122,123,126,131,135,138,143,158,160,161,163,164,165,166,168,171,178,188,192,197],[3, 33, 63, 93, 123, 122, 120, 181, 211, 212, 213, 214, 243, 273, 303, 333, 363, 393, 423, 453, 483, 573, 271, 331, 391, 451, 541, 542, 543, 336, 366, 396, 426, 456, 486, 516, 337, 367, 397, 427, 457, 487, 517, 215, 217, 218, 219, 220, 250, 280, 340, 370, 400, 460, 520, 550, 580, 461, 462, 463, 492, 522, 584, 554, 464, 466, 465, 556, 558, 588, 561, 591, 559, 467, 469, 470, 471, 472, 502, 532, 562, 592, 185, 125, 126, 127, 97, 67, 37, 68, 69, 70, 40, 10, 71, 72, 102, 132, 221, 133,35, 105, 136, 137, 138, 139, 140, 141, 111, 81, 51, 21, 223, 224, 225, 196, 168, 200, 226, 227, 229, 228, 230, 202, 172, 261, 260, 262, 142, 282, 312, 342, 372, 402, 403, 404, 405, 406, 376, 346, 316, 286, 285, 284, 314, 344, 408, 378, 348, 318, 291, 321, 441, 380, 379, 382, 383, 384, 386, 387, 388, 417, 447, 477, 507, 537, 567, 594, 564, 565, 535, 505, 474, 475, 508, 509, 354, 324, 294, 264, 234, 204, 174, 144, 114, 84, 52, 22, 23, 24, 25, 26, 56, 86, 146, 147, 148, 118, 88,119, 149, 205, 206, 207, 208, 299, 298, 297, 296, 327, 328, 235, 267]];
+// Declare global functions
+
+//Check for DOM loaded
+$(function() {
+  // run DOM related functions
+  init();
+});
+function activateMovement() {
+  console.log('activating Movement');
+  $('body').keydown(passKey);
+}
+function deactivateMovement() {
+  $('body').off('keydown');
+}
+function passKey(e) {
+  player.move(e.keyCode);
+}
+function init(){
+  console.log('DOM loaded');
+  // grab DOM-related variables
+  $board = $('.gameboard');
+  $newGame = $('#newgame');
+  $soundButton = $('#sound');
+  music = document.querySelector('#backgroundmusic');
+  eventSound = document.querySelector('#eventsound');
+  $healthBar = $('#healthbar');
+  // run Functions
+
+  // add Event listeners
+  $newGame.on('click', newGame);
+  $soundButton.on('click', toggleMusic);
+  $('#mapmaker').on('click', activateMapEditor);
+  $('#savemap').on('click', saveMap);
+}
+// Declare DOM-related Functions
 class Character {
   constructor(properties) {
     Object.assign(this, properties);
@@ -142,94 +221,12 @@ class Character {
     checkForWin();
   }
 }
-const itemDefinitions = [
-  {
-    name: 'door',
-    imageSrc: '/images/door1.png'
-  },
-  {
-    type: 'weapon',
-    name: 'dagger',
-    damage: 1,
-    imageSrc: '/images/dagger.png'
-  },
-  {
-    type: 'weapon',
-    name: 'sword',
-    damage: 2,
-    imageSrc: '/images/sword.png'
-  },
-  {
-    type: 'armor',
-    name: 'leather',
-    armor: 1,
-    imageSrc: '/images/leather_armor.png'
-  },
-  {
-    type: 'armor',
-    name: 'plate',
-    armor: 2,
-    imageSrc: '/images/plate_armor.png'
-  },
-  {
-    name: 'potion',
-    health: 1,
-    imageSrc: '/images/health1.png'
-  },
-  {
-    type: 'items',
-    name: 'coin',
-    wealth: 1,
-    imageSrc: 'images/coin1.png'
-  }
-
-];
 class Item {
   constructor(properties) {
     Object.assign(this, properties);
     this.location = getCharacterLocation();
   }
 }
-const walls = [[2,4,9,11,12,14,18,24,27,32,33,37,38,46,47,52,59,60,62,63,64,66,72,76,79,84,86,91,92,94],[3,6,9,15,23,28,33,34,35,36,37,41,44,47,57,59,63,64,66,67,72,77,82,91,93,99,105,109,110,111,114,120,122,123,126,131,135,138,143,158,160,161,163,164,165,166,168,171,178,188,192,197],[3, 33, 63, 93, 123, 122, 120, 181, 211, 212, 213, 214, 243, 273, 303, 333, 363, 393, 423, 453, 483, 573, 271, 331, 391, 451, 541, 542, 543, 336, 366, 396, 426, 456, 486, 516, 337, 367, 397, 427, 457, 487, 517, 215, 217, 218, 219, 220, 250, 280, 340, 370, 400, 460, 520, 550, 580, 461, 462, 463, 492, 522, 584, 554, 464, 466, 465, 556, 558, 588, 561, 591, 559, 467, 469, 470, 471, 472, 502, 532, 562, 592, 185, 125, 126, 127, 97, 67, 37, 68, 69, 70, 40, 10, 71, 72, 102, 132, 221, 133,35, 105, 136, 137, 138, 139, 140, 141, 111, 81, 51, 21, 223, 224, 225, 196, 168, 200, 226, 227, 229, 228, 230, 202, 172, 261, 260, 262, 142, 282, 312, 342, 372, 402, 403, 404, 405, 406, 376, 346, 316, 286, 285, 284, 314, 344, 408, 378, 348, 318, 291, 321, 441, 380, 379, 382, 383, 384, 386, 387, 388, 417, 447, 477, 507, 537, 567, 594, 564, 565, 535, 505, 474, 475, 508, 509, 354, 324, 294, 264, 234, 204, 174, 144, 114, 84, 52, 22, 23, 24, 25, 26, 56, 86, 146, 147, 148, 118, 88,119, 149, 205, 206, 207, 208, 299, 298, 297, 296, 327, 328, 235, 267]];
-let visibleSquares = [];
-let stepsTaken = 0;
-
-
-//Check for DOM loaded
-$(function() {
-  // run DOM related functions
-  init();
-});
-function activateMovement() {
-  console.log('activating Movement');
-  $('body').keydown(passKey);
-}
-function deactivateMovement() {
-  $('body').off('keydown');
-}
-function passKey(e) {
-  player.move(e.keyCode);
-}
-function init(){
-  console.log('DOM loaded');
-  // grab DOM-related variables
-  $board = $('.gameboard');
-  $newGame = $('#newgame');
-  $soundButton = $('#sound');
-  music = document.querySelector('#backgroundmusic');
-  eventSound = document.querySelector('#eventsound');
-  $healthBar = $('#healthbar');
-  // run Functions
-
-  // add Event listeners
-  $newGame.on('click', newGame);
-  $soundButton.on('click', toggleMusic);
-  $('#mapmaker').on('click', activateMapEditor);
-  $('#savemap').on('click', saveMap);
-
-}
-
-// Declare DOM-related Functions
 
 function createMap(height,width) {
   $board.html('');
@@ -242,7 +239,23 @@ function createMap(height,width) {
   addWalls();
 
 }
+function addWalls() {
+  walls[level].forEach((location) => {
+    $(`[data-location="${location}"]`).removeClass('floor');
+    $(`[data-location="${location}"]`).addClass('wall');
+  } );
+}
 
+function toggleMusic(e) {
+  e.preventDefault();
+  if (music.paused === false) {
+    music.pause();
+    console.log('music paused');
+  } else {
+    music.play();
+    console.log('music playing');
+  }
+}
 function activateMapEditor() {
   // toggle this
   var clicks = $(this).data('clicks');
@@ -255,9 +268,6 @@ function activateMapEditor() {
   }
   $(this).data('clicks', !clicks);
 }
-function saveMap() {
-  console.log(walls[level]);
-}
 function mapEditor() {
   console.log('I\'m a wall now.');
   $(this).removeClass('floor');
@@ -265,11 +275,50 @@ function mapEditor() {
   console.log($(this).data('location'));
   walls[level].push($(this).data('location'));
 }
-function addWalls() {
-  walls[level].forEach((location) => {
-    $(`[data-location="${location}"]`).removeClass('floor');
-    $(`[data-location="${location}"]`).addClass('wall');
-  } );
+function saveMap() {
+  console.log(walls[level]);
+}
+
+function newGame() {
+  deactivateMovement();
+  createMap(mapDimensions[level][0],mapDimensions[level][1]);
+  reset();
+  startingHealth();
+  // spawn player
+  spawnPlayer();
+  // spawn door
+  spawnDoor();
+  // spawn items
+  spawnItems();
+  //spawn Enemies - move this to object library
+  if (level === 1) {
+    spawnEnemies(3);
+  }
+  if (level === 2) {
+    spawnEnemies(6);
+  }
+  changeVisibility();
+  // Toggle Event Listeners for movement
+  activateMovement();
+}
+function spawnPlayer() {
+  // create player object
+  player = new Character(characterDefinitions[0]);
+  // place player on Map
+  $(`[data-location="${player.location}"]`).html(`<img src=${player.imageSrc}>`);
+}
+function spawnDoor() {
+  door = new Item(itemDefinitions[0]);
+  // ensure door is not too close to the Player
+  console.log(door.location);
+  console.log(player.location);
+  console.log(Math.abs((player.location-door.location)));
+  while (Math.abs((player.location-door.location)) < 49) {
+    console.log('moving door further from player');
+    door.location = getCharacterLocation();
+  }
+  // place door on Map
+  $(`[data-location="${door.location}"]`).html(`<img src=${door.imageSrc}>`);
 }
 function spawnEnemies(amount,level = 1) {
   // create enemies
@@ -306,6 +355,7 @@ function spawnItems(){
   }
 
 }
+
 function getCharacterLocation() {
   let enemyLocation = Math.floor((Math.random() * boardSize));
   while (walls[level].includes(enemyLocation)) {
@@ -315,31 +365,13 @@ function getCharacterLocation() {
   enemyLocations.push[enemyLocation];
   return enemyLocation;
 }
-function newGame() {
-  deactivateMovement();
-  createMap(mapDimensions[level][0],mapDimensions[level][1]);
-  reset();
-  startingHealth();
-  // create player object
-  player = new Character(characterDefinitions[0]);
-  // place player on Map
-  $(`[data-location="${player.location}"]`).html(`<img src=${player.imageSrc}>`);
-  // spawn door
-  door = new Item(itemDefinitions[0]);
-  // place door on Map
-  $(`[data-location="${door.location}"]`).html(`<img src=${door.imageSrc}>`);
-  //spawn Enemies
-  spawnItems();
-  if (level === 1) {
-    spawnEnemies(3);
+function startingHealth() {
+  $healthBar.html('');
+  for (let i = 0; i < 3; i++) {
+    $healthBar.append('<img src="/images/life.png" alt="A Heart">');
   }
-  if (level === 2) {
-    spawnEnemies(6);
-  }
-  changeVisibility();
-  // Toggle Event Listeners for movement
-  activateMovement();
 }
+
 function changeVisibility() {
   visibleSquares = [];
   visibleSquares.push(player.location,visionTop(),visionBottom(),visionLeft(),visionRight());
@@ -364,6 +396,18 @@ function visionRight() {
   if (player.location % boardHeight === boardHeight - 1) return;
   else return player.location + 1;
 }
+
+function combatSound() {
+  eventSound.src = '/sounds/combat.wav';
+  if (eventSound.paused === false) {
+    eventSound.pause();
+    console.log('eventSound paused');
+  } else {
+    eventSound.play();
+    console.log('eventSound playing');
+  }
+}
+
 function checkForWin() {
   if (player.location === door.location) {
     if (level === 2) {
@@ -403,30 +447,4 @@ function reset() {
   stepsTaken = 0;
   enemies = [];
   enemyLocations= [];
-}
-function startingHealth() {
-  $healthBar.html('');
-  for (let i = 0; i < 3; i++) {
-    $healthBar.append('<img src="/images/life.png" alt="A Heart">');
-  }
-}
-function toggleMusic(e) {
-  e.preventDefault();
-  if (music.paused === false) {
-    music.pause();
-    console.log('music paused');
-  } else {
-    music.play();
-    console.log('music playing');
-  }
-}
-function combatSound() {
-  eventSound.src = '/sounds/combat.wav';
-  if (eventSound.paused === false) {
-    eventSound.pause();
-    console.log('eventSound paused');
-  } else {
-    eventSound.play();
-    console.log('eventSound playing');
-  }
 }
